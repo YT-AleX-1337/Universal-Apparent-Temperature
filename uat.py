d4ee1a1872dcfe13044309a890a2d814030387d8
AleX-1337 <90471968+YT-AleX-1337@users.noreply.github.com>
2024-12-30 17:27:41 +0100

    Update uat.py

--- uat.py
+++ uat.py
@@ -10,7 +10,7 @@ W = min(max(int(float(input('Enter wind speed (km/h): '))), 0), 250) #Cap wind s
 H_A = 0.061 * H * exp(17.27 * T / (237.7 + T)) #Calculate water vapor pressure (absolute humidity)
 h = 1 - 2 / (1 + exp(0.2 * T - 2)) #Calculate humidity factor: it should increase apparent temperature when it's hot but slightly decrease it when it's cold
 T_WC = 13.12 - 0.379 * T - (11.37 - 0.397 * T) * (W + 1) ** 0.16 #Calculate wind effect (it lowers the apparent temperature when it's cold and slightly increase it when it's hot)
-w = 1.5 / (1 + exp(0.1 * T + (W + 1) ** 0.16 - 4)) - 0.5 #Calculate wind factor: the higher the temperature, the least effect wind has
+w = 1.2 / (1 + exp(0.1 * T + (W + 1) ** 0.16 - 4)) - 1 / (1 + exp(0.017 * T + 3)) - 0.2 #Calculate wind factor: the higher the temperature, the least effect wind has
 
 T_A = round(T + 0.174 * h * H_A + w * T_WC) #Calculate Universal Apparent Temperature
 print(f'Feels like {T_A}°C')
